70616c4137c4834e1b1c429975356f2f71522fd5
moritztng <moritz.thuening@gmail.com>
2020-10-05 12:38:00 +0200

    Replace requirements.txt with setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="prism-style-transfer",
+    version="0.0.1",
+    author="Moritz Thuening",
+    author_email="hello@prism.art",
+    description="Simple Style Transfer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/moritztng/prism",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'torch==1.6.0',
+        'torchvision==0.7.0',
+        'tensorboard==2.3.0',
+        'Pillow==7.2.0'
+    ]
+)
